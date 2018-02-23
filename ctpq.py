151cd1786fd1220c371331af9068bae55be09af1
ultimatile <kaitaiti@live.jp>
2018-02-23 10:30:07 +0900

    fix loading norm data

--- ctpq.py
+++ ctpq.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from time import time
-#from scipy.misc import factorial
+from scipy.misc import factorial
 import sys
 itime=time()
 fig = plt.figure()
@@ -12,52 +12,69 @@ color=lambda a:cmap(float(a))
 l=2
 N=18
 Tmin=1
-Tmax=10
-Tsteps=10
+Tmax=20
+Tsteps=100
 T=np.linspace(Tmin,Tmax,Tsteps)
-realizations=1
-samples=10
-repeats=10
-#realizations=40
-#samples=100
-#repeats=500
+#realizations=1
+#samples=1
+#repeats=1
+realizations=40
+samples=100
+repeats=1000
 steps=300
-SS=np.zeros((samples,steps,6))
-ene=np.zeros((samples,realizations,steps))
-ene2=np.zeros((samples,realizations,steps))
-norm=np.zeros((samples,realizations,steps))
-pnorm=np.zeros((samples,realizations,steps))
-C=np.zeros((realizations,Tsteps))
-std_C=np.zeros((realizations,Tsteps))
+SS=np.zeros((samples,steps,6),dtype=np.float_)
+ene=np.zeros((samples,realizations,steps),dtype=np.float_)
+ene2=np.zeros((samples,realizations,steps),dtype=np.float_)
+norm=np.zeros((samples,realizations,steps),dtype=np.float_)
+pnorm=np.zeros((samples,realizations,steps),dtype=np.float_)
+C=np.zeros((realizations,Tsteps),dtype=np.float_)
+std_C=np.zeros((realizations,Tsteps),dtype=np.float_)
+#old_settings = np.seterr(all='raise')
 for realization in np.arange(realizations):
     print(time()-itime)
     for sample in np.arange(samples):
         SS[sample,:,:]=np.genfromtxt("zvo{}_SS_rand{}.dat".format(realization,sample))#,skip_header=1)
-        pnorm[sample,realization,:]=np.genfromtxt("zvo{}_Norm_rand{}.dat".format(realization,sample))[:,1]#,skip_header=1)
-        norm[sample,realization,0]=pnorm[sample,realization,0]
-        for i in np.arange(1,steps):
-            norm[sample,realization,i]=norm[sample,realization,i-1]*pnorm[sample,realization,i]
-            print(norm[sample,realization,i])
-        ene[sample,realization,:]=SS[sample,:,1]*norm[sample,realization,:]
-        ene2[sample,realization,:]=SS[sample,:,2]*norm[sample,realization,:]
+        norm[sample,realization,:]=np.cumprod(np.genfromtxt("zvo{}_Norm_rand{}.dat".format(realization,sample))[:,1])**2
+        ene[sample,realization,:]=SS[sample,:,1]*(norm[sample,realization,:])
+        ene2[sample,realization,:]=SS[sample,:,2]*(norm[sample,realization,:])
+        #print(ene[sample,realization,:])
+        #ene[sample,realization,:]=SS[sample,:,1]
+        #ene2[sample,realization,:]=SS[sample,:,2]
     index=0
-
     for t in T:
-        C_t=np.zeros(repeats)
+        C_t=np.zeros(repeats,dtype=np.float_)
         beta=1/t
-        cnorm=0
-        cene2=0
-        cene=0
         for repeat in np.arange(repeats):
+            cnorm=0
+            cene2=0
+            cene=0
             id_b=np.random.choice(samples,samples,replace=True)
+            #print(id_b)
             #factor=(beta*N)**2/2
-            factor=1
+            factor=np.exp(-N*beta*l,dtype=np.float_)
+            max_cnorm_k=0
+            max_cene_k=0
+            max_cene2_k=0
             for k in np.arange(steps-1):
-                if factor >sys.float_info.epsilon:
-                    print("cene2",cene2)
-                    cnorm+=factor*((1+l*beta*N/(2*k+1))*norm[id_b,realization,k]-beta/(2*k+1)*ene[id_b,realization,k])
-                    cene+=factor*((1+l*beta*N/(2*k+1))*ene[id_b,realization,k]-beta/(2*k+1)*ene2[id_b,realization,k])
-                    cene2+=factor*((1-l*beta*N/(2*k+1))*ene2[id_b,realization,k]+(l*l*beta*N*N/(2*k+1))*ene[id_b,realization,k]-beta*N*N/(2*k+1)*ene[id_b,realization,k+1])
+                #print(l*l*N*ene[id_b,realization,k]-l*ene2[id_b,realization,k]-N*ene[id_b,realization,k+1])
+                #print("cene",cene)
+                #print("cene2",cene2)
+                #print("cnorm",cnorm)
+                cnorm_k=((1+l*beta*N/(2*k+1))*norm[id_b,realization,k]-beta/(2*k+1)*ene[id_b,realization,k])*factor
+                cene_k=((1+l*beta*N/(2*k+1))*ene[id_b,realization,k]-beta/(2*k+1)*ene2[id_b,realization,k])*factor
+                cene2_k=((1-l*beta*N/(2*k+1))*ene2[id_b,realization,k]+(l*l*beta*N*N/(2*k+1))*ene[id_b,realization,k]-beta*N*N/(2*k+1)*ene[id_b,realization,k+1])*factor
+                #cene2_k=(ene2[id_b,realization,k]+beta*N/(2*k+1)*(l*l*l*N*N*norm[id_b,realization,k]-l*l*N*ene[id_b,realization,k]-l*N*N*norm[id_b,realization,k+1]-N*ene[id_b,realization,k+1]))*factor
+                #print(k,cnorm_k,cene_k,cene2_k)
+                #print(k,cene2_k,norm[id_b,realization,k],ene[id_b,realization,k],norm[id_b,realization,k+1],ene[id_b,realization,k+1],factor)
+                max_cnorm_k=np.max(np.hstack((max_cnorm_k,np.abs(cnorm_k))))
+                max_cene_k=np.max(np.hstack((max_cene_k,np.abs(cene_k))))
+                max_cene2_k=np.max(np.hstack((max_cene2_k,np.abs(cene2_k))))
+                #print("max",max_cnorm_k*sys.float_info.epsilon,max_cene_k*sys.float_info.epsilon,max_cene2_k*sys.float_info.epsilon)
+                if np.max(np.abs(cnorm_k)) < max_cnorm_k*sys.float_info.epsilon and np.max(np.abs(cene_k)) < max_cene_k*sys.float_info.epsilon and np.max(np.abs(cene2_k)) < max_cene2_k*sys.float_info.epsilon:
+                    break
+                cnorm+=cnorm_k
+                cene+=cene_k
+                cene2+=cene2_k
                 factor*=(N*beta)**2/(4*k*k+6*k+2)
             C_t[repeat]=np.mean(cene2)/np.mean(cnorm)-np.mean(cene)**2/(np.mean(cnorm)**2)
             #print(C_t[repeat])
@@ -67,10 +84,10 @@ for realization in np.arange(realizations):
 #print(C)
 print("finish")
 print(time()-itime)
-yerr=np.sqrt(np.sum(std_C**2,axis=0)/realizations)/realizations
-y=np.mean(C,axis=0)
+yerr=np.sqrt(np.sum(std_C**2,axis=0)/realizations)/realizations/N
+y=np.mean(C,axis=0)/N
 np.save("CSHN18GC",np.vstack((T,y,yerr)).T)
-ax.errorbar(T, y,yerr=yerr, fmt='x')
+ax.errorbar(T,y,yerr=yerr, fmt='x')
 sc = ax.scatter(T, y, s=25, marker='x',linewidths=2,label="bootstrap")
 #x=1/np.mean(np.mean(beta,axis=0),axis=0)
 #y=np.mean(np.mean(beta**2*(tene2-tene**2),axis=0),axis=0)/N
